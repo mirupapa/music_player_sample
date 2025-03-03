5f1af07cae0ff4291f2fe9138d700e1310f85f08
mirupapa <mirutiropapa@icloud.com>
2025-03-03 12:11:14 +0900

    feat: Vercel設定ファイルを追加し、環境設定を更新 feat: Svelteアダプタの設定を拡張し、CSRFチェックを無効化 chore: .vercelignoreファイルを追加し、無視するファイルを指定

--- svelte.config.js
+++ svelte.config.js
@@ -12,8 +12,13 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			runtime: 'nodejs20.x'
-		})
+			runtime: 'nodejs20.x',
+			edge: false,
+			split: false
+		}),
+		csrf: {
+			checkOrigin: false
+		}
 	}
 };
 
