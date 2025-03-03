14ca701679940882205e938297356b1b858a7724
mirupapa <mirutiropapa@icloud.com>
2025-03-03 12:03:17 +0900

    feat(package.json): Vercel用のビルドスクリプトを追加 feat(svelte.config.js): Vercelアダプタに変更し、Node.jsランタイムを指定 chore(vercel.json): デプロイ地域を追加 refactor(vite.config.ts): VitestからViteへのインポートを修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 import {svelteTesting} from '@testing-library/svelte/vite'
 
 export default defineConfig({
